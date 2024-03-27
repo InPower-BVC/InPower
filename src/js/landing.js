e2893aa03eecdf7c6f0da2ef07b6496402532a12
RSharmaDevs <158550524+RSharmaDevs@users.noreply.github.com>
2024-03-26 20:25:05 -0600

    landing

--- src/js/landing.js
+++ src/js/landing.js
@@ -21,7 +21,7 @@ function Landing() {
           </h1>
           <p className="feature-content-p-new">Your Online Community for Healing and Empowerment</p>
           <Link to="/invite">
-            <div className="button-container-new">
+            <div style={{margin: "20px 0px 10px 0px"}} className="button-container-new">
                <BubblyButton />
              </div>
            </Link>
