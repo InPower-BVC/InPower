c0502141173fcabd0ad82c1e8ac4804371c2fe81
RSharmaDevs <158550524+RSharmaDevs@users.noreply.github.com>
2024-04-17 00:59:40 -0600

    changes

--- src/js/commentpage_a.js
+++ src/js/commentpage_a.js
@@ -26,12 +26,13 @@ function Magazine() {
   };
 
   return (
-    <div className="container">
+    <div  style={{marginBottom: "40px"}} className="container">
       <div className="magazine">
         <br />
         <img style={{
-          width: "495px",
-          height: "210px",
+          margin:"4px 0px",
+          width: "320px",
+          height: "140px",
           transform: "rotate(-6deg)"
         }} src={magazineFont} /><br></br>
         <h2 className="line">THE LATEST</h2>
@@ -55,7 +56,7 @@ function Magazine() {
     <Link to="/MagazineInformation" onClick={handleVolumeClick} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className='volume-1' style={{width: "507.54px"}}>
         <img style={{ width: "100%", maxWidth: "340px", height: "480.61px", boxShadow: "0.5px 2px 1.5px 1.8px" }} src={magazine1} alt="Volume 1" />
-        <h2>Volume 1</h2>
+        <h2 style={{marginTop: "18px", color:"#ca4a51"}}>Volume 1</h2>
         <p style={{ fontFamily: 'sans-serif', fontWeight: 'bold' }}>EMBRACING ONE'S DISTINCTIVENESS AND AUTHENTICITY</p>
       </div>
     </Link>
@@ -63,7 +64,7 @@ function Magazine() {
     <Link to="/MagazineInformation1" onClick={handleVolumeClick} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className='volume-2' style={{width: "507.54px"}}>
         <img style={{ width: "100%", maxWidth: "340px", height: "481.61px", boxShadow: "0.5px 2px 1.5px 1.8px" }} src={magazine2} alt="Volume 2" />
-        <h2>Volume 2</h2>
+        <h2 style={{marginTop: "18px", color:"#ca4a51"}}>Volume 2</h2>
         <p style={{ fontFamily: 'sans-serif', fontWeight: 'bold' }}>EMBODYING INDIVIDUALITY AND AUTHENTICITY</p>
       </div>
     </Link>
@@ -71,7 +72,7 @@ function Magazine() {
     <Link to="/MagazineInformation2" onClick={handleVolumeClick} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className='volume-3' style={{width: "507.54px"}}>
         <img style={{ width: "100%", maxWidth: "340px", height: "481.61px", boxShadow: "0.5px 2px 1.5px 1.8px" }} src={magazine3} alt="Volume 3" />
-        <h2>Volume 3</h2>
+        <h2 style={{marginTop: "18px", color:"#ca4a51"}}>Volume 3</h2>
         <p style={{ fontFamily: 'sans-serif', fontWeight: 'bold' }}>EMBRACING INDIVIDUALITY & AUTHENTICITY</p>
       </div>
     </Link>
@@ -79,7 +80,7 @@ function Magazine() {
     <Link to="/MagazineInformation3" onClick={handleVolumeClick} style={{ textDecoration: 'none', color: 'inherit' }}>
       <div className='volume-4' style={{width: "507.54px"}}>
         <img style={{ width: "100%", maxWidth: "340px", height: "481.61px", boxShadow: "0.5px 2px 1.5px 1.8px" }} src={magazine4} alt="Volume 4" />
-        <h2>Volume 4</h2>
+        <h2 style={{marginTop: "18px", color:"#ca4a51"}}>Volume 4</h2>
         <p style={{ fontFamily: 'sans-serif', fontWeight: 'bold' }}>FOSTERING INDIVIDUALITY AND GENUINENESS</p>
       </div>
     </Link>
